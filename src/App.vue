09622d23983aca87bd710f4cdec1d85cb3909849
Brenolima03 <brenocslima@outlook.com>
2024-03-07 16:10:45 -0400

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,97 @@
 <template>
-  <TheHeader v-if="showHeader"/>
+  <div>
+    {{ fullName }}
+
+    <h2>Ongoing todos</h2>
+    <div
+      v-for="todo in incompleteTodos"
+      :key="todo.id"
+    >
+      {{ todo.title }}
+    </div>
+    <br><br>
+
+    <h2>Finished todos</h2>
+    <div
+      v-for="todo in completeTodos"
+      :key="todo.id"
+    >
+      {{ todo.title }}
+    </div>
+    <br><br>
+
+    <h2>Todos</h2>
+    <div
+      v-for="todo in todos"
+      :key="todo.id"
+    >
+      <input
+        v-model="todo.completed"
+        type="checkbox">
+      {{ todo.title }}
+    </div>
+  </div>
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue"
-
 export default {
   name: "App",
-  components: {
-    TheHeader
-  },
   data() {
     return {
-      showHeader: true,
-      firstName: "Jon",
-      lastName: "Snow",
-      showName: false,
-      accessLevel: 'admin'
+      user: {
+        firstName: "Jon",
+        lastName: "Snow",
+      },
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
     }
-  }
+  },
+
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+
+    incompleteTodos() {
+      return this.todos.filter(todo => !todo.completed);
+    },
+
+    completeTodos() {
+      return this.todos.filter(todo => todo.completed);
+    }
+  },
+
+  methods: {}
 }
 </script>
 
