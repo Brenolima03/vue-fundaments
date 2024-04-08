70d1961cc1ba1489f998107167628acf266ffef9
Brenolima03 <brenocslima@outlook.com>
2024-04-08 10:19:32 -0400

    props and emit with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,31 @@
+<!-- App.vue -->
 <template>
-  <AppHook v-if="showAppHook"/>
-  <button @click="showAppHook = !showAppHook">Toggle</button>
-  <h4>User</h4>
-  {{ fullName }}
-
-  <button @click="user.first_name = 'Mauricio'">Update</button>
+    <AppButton
+      data-vue="Jon"
+      variant="danger"
+      @update="getUpdate"
+    >Save <template #icon>Icon</template></AppButton>
 </template>
 
 <script>
-  import { ref, computed, watch } from 'vue';
-  import AppHook from "./components/AppHook"
-  export default {
-    name: "App",
+import AppButton from "./components/AppButton";
 
-    components: {
-      AppHook
-    },
-
-    setup() {
-      const user = ref({
-        first_name: "Eunice",
-        last_name: "Lima"
-      })
-
-      const showAppHook = ref(true);
-
-      const fullName = computed(() => {
-        return `${user.value.first_name} ${user.value.last_name}`
-      })
-
-      watch(() => user.value.first_name, () => {
-        console.log("Logic");
-      })
-
-      return {
-        user,
-        fullName,
-        showAppHook
-      }
+export default {
+  name: "App",
+  components: {AppButton},
+  methods: {
+    getUpdate(data) {
+      console.log("getUpdate", data);
     }
   }
+};
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-  }
-
-  nav {
-    padding: 30px;
-  }
-
-  nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  nav a.router-link-exact-active {
-    color: #42b983;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
 </style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="sendData">
+    {{ variant }}
+    <slot></slot>
+    <slot name="icon"></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      variant: {
+        type: String,
+        default: 'Sucess',
+      },
+    },
+    setup(props, { emit }) {
+
+      const sendData = () => {
+        emit("update", props.variant);
+      }
+
+      return {sendData};
+    }
+  }
+</script>
